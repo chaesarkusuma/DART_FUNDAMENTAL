728de4c4305fd7f06847f8bac836ce8d9c45c487
chaesarkusuma <chaesarputra14@gmail.com>
2026-09-08 11:28:15 +0700

    extension

---
+++ oop 8/extension_method.dart
@@ -0,0 +1,32 @@
+/**
+ * Extension Method adalah fitur yang memungkinkan kita
+ * menambahkan Property maupun Methods pada class yang sudah ada
+ * tanpa perlu mengubah source code pada class tersebut
+ */
+
+/**
+ * Mobil
+ * 1. Mesin
+ * 2. Setir
+ * 3. Av
+ * 4. Radio
+ * ==>
+ * ==> Extension Methodnya
+ * Kamera Parking
+ * GPS
+ * Dashcam
+ */
+
+import 'helpers.dart';
+
+/**
+ * extension NamaExtension on NamaClass{}
+ */
+
+void main(){
+  var numbers = [2, 5, 3, 1, 4];
+
+  print(numbers);
+
+  print(numbers.sortAsc());
+}
---
+++ oop 8/helpers.dart
@@ -0,0 +1,27 @@
+extension Sorting on List<int> {
+  List<int> sortAsc() {
+    var list = this;
+    var lenght = list.length;
+
+    for (var i = 0; i < lenght - 1; i++) {
+      int min = i;
+      for (var j = i; j < lenght; j++) {
+        if (list[j] < list[min]) {
+          min = j;
+        }
+      }
+      int temp = list[min];
+      list[min] = list[i];
+      list[i] = temp;
+    }
+
+    return list;
+  }
+
+  // Contoh Helper Currency
+  // 10000000
+  // 1.000.000
+  // 1,000,000
+  // Rp. 1.000.000
+  // Rp, 100,000,000
+}
